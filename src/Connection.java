95b6a39362ad42ca9e79afd9284ea56b0804ee17
jprr <joaopauloresenderodrigues33@gmail.com>
2024-10-08 14:31:14 -0300

    creating & configuring Class Connection

---
+++ src/Connection.java
@@ -0,0 +1,14 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connection {
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://127.0.0.1:3306/fav_movies";
+    private String user = "root";
+    private String password = "XvZV8*5$i0bI";
+
+    protected java.sql.Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(this.driver);
+        return DriverManager.getConnection(this.url, this.user, this.password);
+    }
+}
