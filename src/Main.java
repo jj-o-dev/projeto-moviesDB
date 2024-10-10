149610401fb8e3c0fd70d095a247f2b7048918bb
jprr <joaopauloresenderodrigues33@gmail.com>
2024-10-10 16:52:21 -0300

    adjusting interface and usability

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,52 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         MyConnection myConnection = new MyConnection();
         Connection connection = myConnection.getConnection();
+        Scanner scanner = new Scanner(System.in);
 
-        String sqlSelect = "SELECT * FROM movies";
-        String sqlInsert = "INSERT INTO movies (title, release_year, director, genre) VALUES (?, ?, ?, ?)";
+        String sqlSelect = "SELECT * FROM movies"; // pronto
+        String sqlInsert = "INSERT INTO movies (title, release_year, director, genre) VALUES (?, ?, ?, ?)";// pronto
         String sqlUpdate = "UPDATE movies SET title = 'Godzilla -1' WHERE title = 'Godzilla Minus One'";
         String sqlDelete = "DELETE FROM movies WHERE id = '4'";
 
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-//      PreparedStatement prepStatement = connection.prepareStatement(sqlInsert);
-        ResultSet result = statement.executeQuery(sqlSelect);
-        printResultSet(result);
+        PreparedStatement insertState = connection.prepareStatement(sqlInsert);
 
-        statement.close();
-        result.close();
-        connection.close();
+        ResultSet result = statement.executeQuery(sqlSelect);
+
+        while (true) {
+            printMenu();
+            System.out.print("Digite a operação desejada: ");
+            int operation = scanner.nextInt();
+
+            switch (operation) {
+                case 1:
+                    printResultSet(result);
+                    int r;
+                    do {
+                        System.out.print("digite 1 para voltar ao menu: ");
+                        r = scanner.nextInt();
+                    } while (r != 1);
+//                    clearTerminal();
+                    continue;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("Encerrando...");
+                    statement.close();result.close();connection.close();scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Número inválido");
+                    continue;
+            }
+        }
     }
 
     public static void printLine(int[] columnWidths) {
@@ -80,4 +109,32 @@ public class Main {
         // Imprimir a última linha de rodapé
         printLine(columnWidths);
     }
+
+    public static void printMenu() {
+        System.out.println("--------------- FAV MOVIES DATABASE ---------------");
+        System.out.println();
+        System.out.println("1 - Mostrar filmes salvos");
+        System.out.println("2 - Inserir novo filme");
+        System.out.println("3 - Modificar informação");
+        System.out.println("4 - Remover um filme");
+        System.out.println("5 - Sair");
+        System.out.println("---------------------------------------------------");
+    }
+
+//    public static void clearTerminal() {
+//        try {
+//            // Identifica o sistema operacional
+//            String sistemaOperacional = System.getProperty("os.name");
+//
+//            if (sistemaOperacional.contains("Windows")) {
+//                // Executa o comando 'cls' no CMD para limpar a tela
+//                new ProcessBuilder( "cls").inheritIO().start().waitFor();
+//            } else {
+//                // Executa o comando 'clear' em sistemas UNIX/Linux/Mac
+//                new ProcessBuilder("clear").inheritIO().start().waitFor();
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
