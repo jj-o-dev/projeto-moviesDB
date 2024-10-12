dc09cdf9807d0b65b670ea79e9f80369009a9bd1
jprr <joaopauloresenderodrigues33@gmail.com>
2024-10-12 16:47:02 -0300

    created error handling blocks

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -14,11 +15,11 @@ public class Main {
 
         if (connection.isValid(10)){
             System.out.println(ANSI_GREEN + "Conexão estabelecida" + ANSI_RESET);
-            Thread.sleep(600);
+            Thread.sleep(700);
         }
 
         String sqlSelect = "SELECT * FROM filmes"; // pronto
-        String selectId = "SELECT 1 FROM filmes WHERE id = ?";
+        String selectId = "SELECT 1 FROM filmes WHERE id = ?";// pronto
         String sqlInsert = "INSERT INTO filmes (titulo, ano, diretor, genero) VALUES (?, ?, ?, ?)";// pronto
         String sqlDelete = "DELETE FROM filmes WHERE id = ?";// pronto
 
@@ -26,7 +27,7 @@ public class Main {
         PreparedStatement insertState = connection.prepareStatement(sqlInsert);
         PreparedStatement deleteState = connection.prepareStatement(sqlDelete);
         PreparedStatement idState = connection.prepareStatement(selectId);
-        ResultSet haveID = null;
+        ResultSet haveID;
         int status, id;
 
         while (true) {
@@ -34,28 +35,43 @@ public class Main {
             ResultSet result = selectState.executeQuery(sqlSelect);
             Printers.printMenu();
             System.out.print("Digite a operação desejada: ");
-            int operation = scanner.nextInt();
+            int operation;
+
+            try {
+                operation = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println(ANSI_RED + "Erro:letras e símbolos não são aceitos!" + ANSI_RESET);
+                Thread.sleep(1500);
+                continue;
+            }
 
             switch (operation) {
                 case 1:
                     Printers.clearTerminal();
                     Printers.printResultSet(result);
                     result.close();
-                    int r;
+                    int r = 1;
                     do {
                         System.out.print(ANSI_YELLOW + "digite 0 para voltar ao menu: " + ANSI_RESET);
-                        r = scanner.nextInt();
+                        try {
+                            r = scanner.nextInt();
+                        } catch (InputMismatchException e) {
+                            scanner.nextLine();
+                        }
                     } while (r != 0);
                     continue;
+
                 case 2:
                     Printers.clearTerminal();
                     scanner.nextLine();
+
                     System.out.println("Insira as informações do filme");
                     System.out.print("Título: ");
                     String title = scanner.nextLine();
                     insertState.setString(1, title);
 
-                    System.out.print("Ano de lançamento: ");
+                    System.out.print("Ano de lançamento (4 dígitos) : ");
                     String year = scanner.nextLine();
                     insertState.setString(2, year);
 
@@ -66,69 +82,98 @@ public class Main {
                     System.out.print("gênero: ");
                     String genre = scanner.nextLine();
                     insertState.setString(4, genre);
-                    status = insertState.executeUpdate();
+
+                    try {
+                        status = insertState.executeUpdate();
+                    } catch (Exception e) {
+                        System.out.println(ANSI_RED + "Erro: input inválido!" + ANSI_RESET);
+                        System.out.println(ANSI_RED + "Operação cancelada" + ANSI_RESET);
+                        Thread.sleep(1500);
+                        continue;
+                    }
 
                     if (status > 0) {
                         System.out.println("\n" + ANSI_GREEN + "Filme salvo!" + ANSI_RESET);
                     }
-                    Thread.sleep(700);
+                    Thread.sleep(800);
                     continue;
+
                 case 3:
                     Printers.clearTerminal();
                     Printers.printResultSet(result);
-                    System.out.print(ANSI_YELLOW + "Selecione o id do filme que deseja modificar: " + ANSI_RESET);
-                    id = scanner.nextInt();
-                    scanner.nextLine();
 
+                    try {
+                        System.out.print(ANSI_YELLOW + "Selecione o id do filme que deseja modificar: " + ANSI_RESET);
+                        id = scanner.nextInt();
+                    } catch (InputMismatchException e) {
+                        scanner.nextLine();
+                        System.out.println(ANSI_RED + "Erro: input inválido!" + ANSI_RESET);
+                        System.out.println(ANSI_RED + "Operação cancelada" + ANSI_RESET);
+                        Thread.sleep(1500);
+                        continue;
+                    }
+
+                    scanner.nextLine();
                     idState.setInt(1, id);
                     haveID = idState.executeQuery();
                     if (!(haveID.next())) {
                         System.out.println(ANSI_RED + "\nid não encontrado" + ANSI_RESET);
-                        Thread.sleep(500);
+                        Thread.sleep(800);
                         continue;
                     }
 
-                    boolean x;
+                    boolean validate = false;
                     String column = null;
                     String newValue = null;
 
                     do {
                         Printers.clearTerminal();
-                        System.out.println("Agora selecione qual dos campos quer editar:");
-                        Printers.printColumns();
-                        int option = scanner.nextInt();
+                        int option;
+                        try {
+                            System.out.println("Agora selecione qual dos campos quer editar:");
+                            Printers.printColumns();
+                            option = scanner.nextInt();
+                        } catch (InputMismatchException e) {
+                            scanner.nextLine();
+                            System.out.println(ANSI_RED + "Erro: input inválido!" + ANSI_RESET);
+                            System.out.println(ANSI_RED +
+                                    "Número inválido: digite apenas entre 1 - 4\n" + ANSI_RESET);
+                            Thread.sleep(1500);
+                            continue;
+                        }
+
                         scanner.nextLine();
                         switch (option) {
                             case 1:
                                 column = "titulo";
                                 System.out.print("Digite o novo título: ");
                                 newValue = scanner.nextLine();
-                                x = true;
+                                validate = true;
                                 break;
                             case 2:
                                 column = "ano";
                                 System.out.print("Digite o novo ano: ");
                                 newValue = scanner.nextLine();
-                                x = true;
+                                validate = true;
                                 break;
                             case 3:
                                 column = "diretor";
                                 System.out.print("Digite o novo nome: ");
                                 newValue = scanner.nextLine();
-                                x = true;
+                                validate = true;
                                 break;
                             case 4:
                                 column = "genero";
                                 System.out.print("Digite o novo gênero: ");
                                 newValue = scanner.nextLine();
-                                x = true;
+                                validate = true;
                                 break;
                             default:
                                 System.out.println(ANSI_RED +
-                                        "Número inválido: digite apenas entre 1 - 5\n" + ANSI_RESET);
-                                x = false;
+                                        "Número inválido: digite apenas entre 1 - 4\n" + ANSI_RESET);
+                                Thread.sleep(1500);
                         }
-                    } while (!x);
+                    } while (!validate);
 
                     String sqlUpdate = "UPDATE filmes SET " + column + " = ? WHERE id = ?"; // pronto
                     PreparedStatement updateState = connection.prepareStatement(sqlUpdate);
@@ -140,20 +185,30 @@ public class Main {
                     if (status > 0) {
                         System.out.println("\n" + ANSI_GREEN + "Atualizado!" + ANSI_RESET);
                     }
-                    Thread.sleep(600);
+                    Thread.sleep(800);
                     continue;
+
                 case 4:
                     Printers.clearTerminal();
                     Printers.printResultSet(result);
                     result.close();
-                    System.out.print(ANSI_YELLOW + "\nDigite o id do filme que deseja remover: " + ANSI_RESET);
-                    id = scanner.nextInt();
+
+                    try {
+                        System.out.print(ANSI_YELLOW + "\nDigite o id do filme que deseja remover: " + ANSI_RESET);
+                        id = scanner.nextInt();
+                    } catch (InputMismatchException e) {
+                        scanner.nextLine();
+                        System.out.println(ANSI_RED + "Erro: input inválido!" + ANSI_RESET);
+                        System.out.println(ANSI_RED + "Operação cancelada" + ANSI_RESET);
+                        Thread.sleep(1500);
+                        continue;
+                    }
 
                     idState.setInt(1, id);
                     haveID = idState.executeQuery();
                     if (!(haveID.next())) {
                         System.out.println(ANSI_RED + "\nid não encontrado" + ANSI_RESET);
-                        Thread.sleep(500);
+                        Thread.sleep(1000);
                         continue;
                     }
 
@@ -162,14 +217,16 @@ public class Main {
                     if (status > 0) {
                         System.out.println("\n" + ANSI_GREEN + "Filme removido!" + ANSI_RESET);
                     }
-                    Thread.sleep(600);
+                    Thread.sleep(800);
                     continue;
+
                 case 5:
                     System.out.println("\nEncerrando...");
                     selectState.close();insertState.close();deleteState.close();
                     connection.close();scanner.close();
                     Thread.sleep(500);
                     System.exit(0);
+
                 default:
                     System.out.println(ANSI_RED + "Número inválido: digite apenas entre 1 - 5\n" + ANSI_RESET);
                     Thread.sleep(1500);
