9393a5671f5b590d9a567ffc2b7ea851223b8843
jprr <joaopauloresenderodrigues33@gmail.com>
2024-10-11 21:35:53 -0300

    created class Printers finalized all 4 operations + errors treated some adjustments

--- src/Main.java
+++ src/Main.java
@@ -2,140 +2,179 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
         MyConnection myConnection = new MyConnection();
         Connection connection = myConnection.getConnection();
-        Scanner scanner = new Scanner(System.in);
 
-        String sqlSelect = "SELECT * FROM movies"; // pronto
-        String sqlInsert = "INSERT INTO movies (title, release_year, director, genre) VALUES (?, ?, ?, ?)";// pronto
-        String sqlUpdate = "UPDATE movies SET title = 'Godzilla -1' WHERE title = 'Godzilla Minus One'";
-        String sqlDelete = "DELETE FROM movies WHERE id = '4'";
+        if (connection.isValid(10)){
+            System.out.println(ANSI_GREEN + "Conexão estabelecida" + ANSI_RESET);
+            Thread.sleep(600);
+        }
 
-        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        String sqlSelect = "SELECT * FROM filmes"; // pronto
+        String selectId = "SELECT 1 FROM filmes WHERE id = ?";
+        String sqlInsert = "INSERT INTO filmes (titulo, ano, diretor, genero) VALUES (?, ?, ?, ?)";// pronto
+        String sqlDelete = "DELETE FROM filmes WHERE id = ?";// pronto
+
+        Statement selectState = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         PreparedStatement insertState = connection.prepareStatement(sqlInsert);
-
-        ResultSet result = statement.executeQuery(sqlSelect);
+        PreparedStatement deleteState = connection.prepareStatement(sqlDelete);
+        PreparedStatement idState = connection.prepareStatement(selectId);
+        ResultSet haveID = null;
+        int status, id;
 
         while (true) {
-            printMenu();
+            Printers.clearTerminal();
+            ResultSet result = selectState.executeQuery(sqlSelect);
+            Printers.printMenu();
             System.out.print("Digite a operação desejada: ");
             int operation = scanner.nextInt();
 
             switch (operation) {
                 case 1:
-                    clearTerminal();
-                    printResultSet(result);
+                    Printers.clearTerminal();
+                    Printers.printResultSet(result);
+                    result.close();
                     int r;
                     do {
-                        System.out.print("digite 1 para voltar ao menu: ");
+                        System.out.print(ANSI_YELLOW + "digite 0 para voltar ao menu: " + ANSI_RESET);
                         r = scanner.nextInt();
-                    } while (r != 1);
-                    clearTerminal();
+                    } while (r != 0);
                     continue;
                 case 2:
-                    break;
+                    Printers.clearTerminal();
+                    scanner.nextLine();
+                    System.out.println("Insira as informações do filme");
+                    System.out.print("Título: ");
+                    String title = scanner.nextLine();
+                    insertState.setString(1, title);
+
+                    System.out.print("Ano de lançamento: ");
+                    String year = scanner.nextLine();
+                    insertState.setString(2, year);
+
+                    System.out.print("Nome do diretor(a): ");
+                    String dirName = scanner.nextLine();
+                    insertState.setString(3, dirName);
+
+                    System.out.print("gênero: ");
+                    String genre = scanner.nextLine();
+                    insertState.setString(4, genre);
+                    status = insertState.executeUpdate();
+
+                    if (status > 0) {
+                        System.out.println("\n" + ANSI_GREEN + "Filme salvo!" + ANSI_RESET);
+                    }
+                    Thread.sleep(700);
+                    continue;
                 case 3:
-                    break;
+                    Printers.clearTerminal();
+                    Printers.printResultSet(result);
+                    System.out.print(ANSI_YELLOW + "Selecione o id do filme que deseja modificar: " + ANSI_RESET);
+                    id = scanner.nextInt();
+                    scanner.nextLine();
+
+                    idState.setInt(1, id);
+                    haveID = idState.executeQuery();
+                    if (!(haveID.next())) {
+                        System.out.println(ANSI_RED + "\nid não encontrado" + ANSI_RESET);
+                        Thread.sleep(500);
+                        continue;
+                    }
+
+                    boolean x;
+                    String column = null;
+                    String newValue = null;
+
+                    do {
+                        Printers.clearTerminal();
+                        System.out.println("Agora selecione qual dos campos quer editar:");
+                        Printers.printColumns();
+                        int option = scanner.nextInt();
+                        scanner.nextLine();
+                        switch (option) {
+                            case 1:
+                                column = "titulo";
+                                System.out.print("Digite o novo título: ");
+                                newValue = scanner.nextLine();
+                                x = true;
+                                break;
+                            case 2:
+                                column = "ano";
+                                System.out.print("Digite o novo ano: ");
+                                newValue = scanner.nextLine();
+                                x = true;
+                                break;
+                            case 3:
+                                column = "diretor";
+                                System.out.print("Digite o novo nome: ");
+                                newValue = scanner.nextLine();
+                                x = true;
+                                break;
+                            case 4:
+                                column = "genero";
+                                System.out.print("Digite o novo gênero: ");
+                                newValue = scanner.nextLine();
+                                x = true;
+                                break;
+                            default:
+                                System.out.println(ANSI_RED +
+                                        "Número inválido: digite apenas entre 1 - 5\n" + ANSI_RESET);
+                                x = false;
+                        }
+                    } while (!x);
+
+                    String sqlUpdate = "UPDATE filmes SET " + column + " = ? WHERE id = ?"; // pronto
+                    PreparedStatement updateState = connection.prepareStatement(sqlUpdate);
+
+                    updateState.setString(1, newValue);
+                    updateState.setInt(2, id);
+                    status = updateState.executeUpdate();
+
+                    if (status > 0) {
+                        System.out.println("\n" + ANSI_GREEN + "Atualizado!" + ANSI_RESET);
+                    }
+                    Thread.sleep(600);
+                    continue;
                 case 4:
-                    break;
+                    Printers.clearTerminal();
+                    Printers.printResultSet(result);
+                    result.close();
+                    System.out.print(ANSI_YELLOW + "\nDigite o id do filme que deseja remover: " + ANSI_RESET);
+                    id = scanner.nextInt();
+
+                    idState.setInt(1, id);
+                    haveID = idState.executeQuery();
+                    if (!(haveID.next())) {
+                        System.out.println(ANSI_RED + "\nid não encontrado" + ANSI_RESET);
+                        Thread.sleep(500);
+                        continue;
+                    }
+
+                    deleteState.setInt(1, id);
+                    status = deleteState.executeUpdate();
+                    if (status > 0) {
+                        System.out.println("\n" + ANSI_GREEN + "Filme removido!" + ANSI_RESET);
+                    }
+                    Thread.sleep(600);
+                    continue;
                 case 5:
-                    System.out.println("Encerrando...");
-                    statement.close();result.close();connection.close();scanner.close();
+                    System.out.println("\nEncerrando...");
+                    selectState.close();insertState.close();deleteState.close();
+                    connection.close();scanner.close();
+                    Thread.sleep(500);
                     System.exit(0);
                 default:
-                    System.out.println("Número inválido");
-                    continue;
+                    System.out.println(ANSI_RED + "Número inválido: digite apenas entre 1 - 5\n" + ANSI_RESET);
+                    Thread.sleep(1500);
+                    Printers.clearTerminal();
             }
-        }
-    }
-
-    public static void printLine(int[] columnWidths) {
-        for (int width : columnWidths) {
-            System.out.print("+");
-            System.out.print("-".repeat(width + 2));
-        }
-        System.out.println("+");
-    }
-
-    public static void printResultSet(ResultSet rs) throws SQLException {
-        // Obter os metadados do ResultSet
-        ResultSetMetaData rsmd = rs.getMetaData();
-        int columnCount = rsmd.getColumnCount();
-
-        // Definir as larguras de cada coluna (pode ser ajustado manualmente)
-        int[] columnWidths = new int[columnCount];
-        for (int i = 1; i <= columnCount; i++) {
-            if (rsmd.getColumnName(i).equalsIgnoreCase("id")) {
-                columnWidths[i - 1] = 5;
-            } else {
-                // Para as outras colunas, calculamos dinamicamente
-                columnWidths[i - 1] = Math.max(rsmd.getColumnName(i).length(), 15); // 15 é o tamanho mínimo para outras colunas
-            }
-        }
-
-        // Ajustar largura das colunas de acordo com os dados
-        while (rs.next()) { // Itera normalmente para calcular a largura das colunas
-            for (int i = 1; i <= columnCount; i++) {
-                String value = rs.getString(i);
-                if (value != null) {
-                    columnWidths[i - 1] = Math.max(columnWidths[i - 1], value.length());
-                }
-            }
-        }
-        rs.beforeFirst(); // Voltar ao início novamente
-
-        // Imprimir a linha de cabeçalho
-        printLine(columnWidths);
-        for (int i = 1; i <= columnCount; i++) {
-            System.out.printf("| %-"+ (columnWidths[i - 1] + 1) +"s", rsmd.getColumnName(i));
-        }
-        System.out.println("|");
-
-        // Imprimir a linha separadora
-        printLine(columnWidths);
-
-        // Imprimir os dados do ResultSet
-        while (rs.next()) {
-            for (int i = 1; i <= columnCount; i++) {
-                String value = rs.getString(i);
-                if (value == null) {
-                    value = ""; // Tratar valores nulos
-                }
-                System.out.printf("| %-"+ (columnWidths[i - 1] + 1) +"s", value);
-            }
-            System.out.println("|");
-        }
-
-        // Imprimir a última linha de rodapé
-        printLine(columnWidths);
-    }
-
-    public static void printMenu() {
-        System.out.println("--------------- FAV MOVIES DATABASE ---------------");
-        System.out.println();
-        System.out.println("1 - Mostrar filmes salvos");
-        System.out.println("2 - Inserir novo filme");
-        System.out.println("3 - Modificar informação");
-        System.out.println("4 - Remover um filme");
-        System.out.println("5 - Sair");
-        System.out.println("---------------------------------------------------");
-    }
-
-    public static void clearTerminal() {
-        try {
-            // Identifica o sistema operacional
-            String sistemaOperacional = System.getProperty("os.name");
-
-            if (sistemaOperacional.contains("Windows")) {
-                // Executa o comando 'cls' no CMD para limpar a tela
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                // Executa o comando 'clear' em sistemas UNIX/Linux/Mac
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
---
+++ src/Printers.java
@@ -0,0 +1,105 @@
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+public class Printers {
+
+    public static void printLine(int[] columnWidths) {
+        for (int width : columnWidths) {
+            System.out.print("+");
+            System.out.print("-".repeat(width + 2));
+        }
+        System.out.println("+");
+    }
+
+    public static void printResultSet(ResultSet rs) throws SQLException {
+        // Obter os metadados do ResultSet
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+
+        // Definir as larguras de cada coluna (pode ser ajustado manualmente)
+        int[] columnWidths = new int[columnCount];
+        for (int i = 1; i <= columnCount; i++) {
+            if (rsmd.getColumnName(i).equalsIgnoreCase("id") || rsmd.getColumnName(i).equalsIgnoreCase("release_year")) {
+                columnWidths[i - 1] = 4;
+            } else {
+                // Para as outras colunas, calculamos dinamicamente
+                columnWidths[i - 1] = Math.max(rsmd.getColumnName(i).length(), 18); // 15 é o tamanho mínimo para outras colunas
+            }
+        }
+
+        // Ajustar largura das colunas de acordo com os dados
+        while (rs.next()) { // Itera normalmente para calcular a largura das colunas
+            for (int i = 1; i <= columnCount; i++) {
+                String value = rs.getString(i);
+                if (value != null) {
+                    columnWidths[i - 1] = Math.max(columnWidths[i - 1], value.length());
+                }
+            }
+        }
+        rs.beforeFirst(); // Voltar ao início novamente
+
+        // Imprimir a linha de cabeçalho
+        printLine(columnWidths);
+        for (int i = 1; i <= columnCount; i++) {
+            System.out.printf("| %-"+ (columnWidths[i - 1] + 1) +"s", rsmd.getColumnName(i));
+        }
+        System.out.println("|");
+
+        // Imprimir a linha separadora
+        printLine(columnWidths);
+
+        // Imprimir os dados do ResultSet
+        while (rs.next()) {
+            for (int i = 1; i <= columnCount; i++) {
+                String value = rs.getString(i);
+                if (value == null) {
+                    value = ""; // Tratar valores nulos
+                }
+                System.out.printf("| %-"+ (columnWidths[i - 1] + 1) +"s", value);
+            }
+            System.out.println("|");
+        }
+
+        // Imprimir a última linha de rodapé
+        printLine(columnWidths);
+    }
+
+    public static void printMenu() {
+        System.out.println("--------------- FAV MOVIES DATABASE ---------------");
+        System.out.println();
+        System.out.println("1 - Mostrar filmes salvos");
+        System.out.println("2 - Inserir novo filme");
+        System.out.println("3 - Modificar informação");
+        System.out.println("4 - Remover um filme");
+        System.out.println("5 - Sair");
+        System.out.println("---------------------------------------------------");
+    }
+
+    public static void printColumns() {
+        System.out.println("----------------------");
+        System.out.println("1 - Título");
+        System.out.println("2 - Ano");
+        System.out.println("3 - Nome do diretor(a)");
+        System.out.println("4 - gênero");
+        System.out.println("----------------------");
+    }
+
+    public static void clearTerminal() {
+        try {
+            // Identifica o sistema operacional
+            String sistemaOperacional = System.getProperty("os.name");
+
+            if (sistemaOperacional.contains("Windows")) {
+                // Executa o comando 'cls' no CMD para limpar a tela
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                // Executa o comando 'clear' em sistemas UNIX/Linux/Mac
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
