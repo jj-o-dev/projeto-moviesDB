3dfb0fd6bd581b18949e98a4f179410f2f3bcfba
jprr <joaopauloresenderodrigues33@gmail.com>
2024-12-16 10:29:03 -0300

    update database

--- src/MyConnection.java
+++ src/MyConnection.java
@@ -4,10 +4,10 @@ import java.util.Base64;
 
 public class MyConnection {
 
-    private final String info1 = "c3FsMTA3Mzc0MDM=";
-    private final String info2 = "NEd5UElCV3Zudw==";
+    private final String info1 = "c3FsMTA3NTIzODE=";
+    private final String info2 = "WUg1ZElLQnBBcg==";
     private final String driver = "com.mysql.cj.jdbc.Driver";
-    private final String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10737403";
+    private final String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10752381";
     private final String user = new String(Base64.getDecoder().decode(info1));
     private final String password = new String(Base64.getDecoder().decode(info2));
 
